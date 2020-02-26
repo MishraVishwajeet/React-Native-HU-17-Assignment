cbb238d12253dd86b4a77112bd87f7e3a84542e0
vishwajeet.mishra <vishwajeet.mishra@hashedin.com>
2020-02-26 23:58:44 +0530

    Login Page UI Completed

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,23 +1,40 @@
 import React,{ Component} from 'react';
-import {StyleSheet,View,Image,Text,TouchableOpacity} from 'react-native';
+import {StyleSheet,View,Image,Text,TouchableOpacity,TextInput} from 'react-native';
 
-export default class Login extends Component{
-    render(){
-        return(
-            <View style={styles.container}>
-                <View style={styles.titleContainer}>
-                <TouchableOpacity style={styles.backButton}>
-                    <Image style={styles.backLogo} source={require('../../Images/back.png')}></Image>
-                </TouchableOpacity>
-                <Text style={styles.textHeader}>LOGIN</Text>
-                </View>
-                <Text style={styles.emailHeader}>EMAIL ADDRESS/PHONE NUMBER</Text>
+ const Login=()=>{
+    const [email,setEmail]=React.useState("eg:sulthan@tradenapp.com or +91 9742 889 859");
+    const [password,setPassword]=React.useState("Password");
+    return(
+        <View style={styles.container}>
+            <View style={styles.titleContainer}>
+            <TouchableOpacity style={styles.backButton}>
+                <Image style={styles.backLogo} source={require('../../Images/back.png')}></Image>
+            </TouchableOpacity>
+            <Text style={styles.textHeader}>LOGIN</Text>
             </View>
-        );
-    }
+            <Text style={styles.emailHeader}>EMAIL ADDRESS/PHONE NUMBER</Text>
+            <TextInput
+                style={styles.emailBody}
+                value={email}
+                onChangeText={text=>setEmail(text)}
+                ></TextInput>
+            <Text style={styles.passwordHeader}>PASSWORD</Text>
+            <TextInput
+                style={styles.passwordBody}
+                value={password}
+                onChangeText={text=>setPassword(text)}
+                ></TextInput>
+                <TouchableOpacity style={styles.loginButton}><Text style={styles.loginTitle}>LOGIN</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.lastButton}><Text style={styles.last}>Forget Password?</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.lastButton}><Text style={styles.last}>Not a Member Yet? Sign Up Now</Text></TouchableOpacity>
+        </View>
+    );
 }
 
 const styles=StyleSheet.create({
+    lastButton:{
+        backgroundColor:"#FFFFFF"
+    },
     container:{
         flex:1,
         backgroundColor:'#FFFFFF'
@@ -26,12 +43,12 @@ const styles=StyleSheet.create({
         marginLeft:30,
         fontSize:12,
         color:"#D8D8D8",
-        marginTop:150,
+        marginTop:120,
     },
     titleContainer:{
         flex:1,
         flexDirection:"row",
-        marginTop:100,
+        marginTop:80,
         maxHeight:50
     },
     textHeader:{
@@ -49,5 +66,47 @@ const styles=StyleSheet.create({
     backLogo:{
         width:20,
         height:20
+    },
+    emailBody:{
+        marginLeft:30,
+        borderBottomColor:"black",
+        borderBottomWidth:2,
+        marginRight:30,
+        fontSize:13
+    },
+    last:{
+        marginLeft:30,
+        fontSize:12,
+        color:"blue",
+        marginTop:10,  
+    },
+    passwordHeader:{
+        marginLeft:30,
+        fontSize:12,
+        color:"#D8D8D8",
+        marginTop:10,
+    },
+    passwordBody:{
+        marginLeft:30,
+        borderBottomColor:"black",
+        borderBottomWidth:2,
+        marginRight:30,
+        fontSize:13
+    },
+    loginButton:{
+        backgroundColor:"blue",
+        marginLeft:40,
+        marginTop:20,
+        marginRight:40,
+        height:40,
+        borderRadius:30
+    },
+    loginTitle:{
+        textAlign:"center",
+        marginTop:10,
+        color:"white"
     }
 });
+
+
+export default Login;
